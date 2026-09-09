0901787cb9d079035d8d1a66836d162c7b06dab2
P-dry <fazolinpedroof@gmail.com>
2026-09-09 15:48:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ const obs = document.getElementById('obs');
 const previewNome = document.getElementById('previewNome');
 const previewAno = document.getElementById('previewAno');
 
-// TROQUE pelo seu número com DDI + DDD, apenas números.
-// Exemplo: 5511999999999
+
 const whatsappNumber = '5511911801381';
 
 function updatePreview() {
